37f2159107383f04074e397480e70de551e73b05
SuLois <louis.lagrange@outlook.fr>
2022-11-28 20:03:21 +0100

    Création de deux fragments : - Code barre - Detection
    Codage des boutons, problème de code, l'application plante à chaque appuie sur bouton frgament scan
     Viens peut être de la Nav barre ?? Ou bien varaible position ?

--- app/src/main/java/com/example/mondechetapp/ScanFragment.java
+++ app/src/main/java/com/example/mondechetapp/ScanFragment.java
@@ -1,64 +1,57 @@
 package com.example.mondechetapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ScanFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ScanFragment extends Fragment {
+public class ScanFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private NavBarFragment.OnButtonClickedListener callback;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    @Override
+    public void onClick(View view) {
+        Log.e(getClass().getSimpleName(),"Button clicked !");
+    }
+
+    public interface OnButtonClickedListener{
+        public void onButtonClicked(int button);
+    }
 
     public ScanFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScanFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ScanFragment newInstance(String param1, String param2) {
-        ScanFragment fragment = new ScanFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan, container, false);
+        View result = inflater.inflate(R.layout.fragment_scan, container, false);
+        result.findViewById(R.id.buttonCodeBarre).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = 1;
+                callback.onButtonClicked(position);
+            }
+        });
+        result.findViewById(R.id.buttonDetection).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = 2;
+                callback.onButtonClicked(position);
+            }
+
+        });
+        return result;
     }
 }
