e42aee65b3735e8a470ae87462e567cba6efdc7b
SuLois <louis.lagrange@outlook.fr>
2022-12-10 13:04:06 +0100

    Ajout d'un fichier de denriere minute (fichier vide "dechets" dans le dosseir 'BDD')

---
+++ app/src/main/java/com/example/mondechetapp/BDD/Dechets.java
@@ -0,0 +1,11 @@
+package com.example.mondechetapp.BDD;
+
+import com.example.mondechetapp.CodeBarreFragment;
+import com.example.mondechetapp.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Dechets {
+
+}
